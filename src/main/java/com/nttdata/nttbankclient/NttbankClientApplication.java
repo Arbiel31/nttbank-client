0d14b199ae82edb2ab9015e4b02695beb83d21ec
Percy <xvppp31@gmail.com>
2022-06-28 19:53:34 -0500

    Added eureka

--- src/main/java/com/nttdata/nttbankclient/NttbankClientApplication.java
+++ src/main/java/com/nttdata/nttbankclient/NttbankClientApplication.java
@@ -2,8 +2,9 @@ package com.nttdata.nttbankclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class NttbankClientApplication {
   public static void main(String[] args) {
